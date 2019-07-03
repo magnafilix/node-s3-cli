25d99b3cf5a72b8cba8983a35430c2fa1620213d
👾 <magnafilix8@gmail.com>
2019-07-04 00:21:53 +0300

    file upload

--- index.js
+++ index.js
@@ -8,10 +8,35 @@ const S3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
-if (process.argv[2] === '-lf')
-  return S3.listObjects({ Bucket: (process.argv[3] || 'lcloud-427-ts') }, (err, files) => {
-    if (err) return console.log(`Error listing files ${err.message && `: ${err.message}`}`.red)
+const FLAG = process.argv[2]
+const NAMEorPATH = process.argv[3]
+
+//---> LISTING FILES FROM A BUCKET
+if (FLAG === '-lf')
+  return S3.listObjects({ Bucket: (NAMEorPATH || 'lcloud-427-ts') }, (err, files) => {
+    if (err) throw err
     console.log(files)
   })
 
+
+if (FLAG === '-uf' && NAMEorPATH)
+  return fs.readFile(NAMEorPATH, (err, file) => {
+    if (err) throw err
+
+    const pathHasSlash = NAMEorPATH.indexOf('/') > -1
+    const fileName = pathHasSlash && NAMEorPATH.split('/').pop()
+
+    const params = {
+      Bucket: 'lcloud-427-ts',
+      Key: (fileName || `file(${new Date().toJSON()})`),
+      Body: JSON.stringify(file, null, 2)
+    }
+
+    S3.upload(params, (err, data) => {
+      if (err) throw err
+      console.log(`File uploaded successfully at ${data.Location}`.bgGreen)
+    })
+  })
+
+//---> WRONG COMMAND HANDLER
 console.log(`The command was not recognized`.bgRed)
