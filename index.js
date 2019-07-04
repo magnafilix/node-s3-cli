f5a70fd1f91359f7dac021f5f9f29225264bde4c
👾 <magnafilix8@gmail.com>
2019-07-05 01:42:06 +0300

    list ALL files, filtering/matching

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
-const uuid = require('uuid')
 const AWS = require('aws-sdk')
 const colors = require('colors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const S3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
@@ -12,59 +14,97 @@ const BUCKET_NAME = 'lcloud-427-ts'
 const FLAG = process.argv[2]
 const ARGV_3 = process.argv[3]
 
-//---> LIST BUCKET FILES
-if (FLAG === '--list' && !ARGV_3)
-  return S3.listObjects({ Bucket: BUCKET_NAME }, (err, files) => {
+const listAllBucketFiles = async () => {
+  let isTruncated = true;
+  let marker;
+
+  while (isTruncated) {
+    let params = { Bucket: BUCKET_NAME };
+    if (marker) params.Marker = marker;
+
+    try {
+      const response = await S3.listObjects(params).promise()
+      response.Contents.forEach(item => console.log(`${item.Key}`.cyan))
+
+      isTruncated = response.IsTruncated
+      if (isTruncated) marker = response.Contents.slice(-1)[0].Key
+
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const uploadFileToBucket = () => fs.readFile(ARGV_3, (err, file) => {
+  if (err) throw err
+
+  const pathHasSlash = ARGV_3.indexOf('/') > -1
+  const fileName = pathHasSlash && ARGV_3.split('/').pop()
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: (fileName || `file(${new Date().toJSON()})`),
+    Body: JSON.stringify(file, null, 2)
+  }
+
+  S3.upload(params, (err, data) => {
     if (err) throw err
-    console.log(files)
+    console.log(`File uploaded successfully at ${data.Location}`.bgGreen)
   })
+})
 
-//---> LIST FILES BY PREFIX
-if (FLAG === '--list' && ARGV_3)
-  return S3.listObjects({ Bucket: BUCKET_NAME, Prefix: ARGV_3 }, (err, files) => {
-    if (err) throw err
-    console.log(files)
-  })
+const listFilesByMatch = async () => {
+  let isTruncated = true;
+  let marker;
 
-//---> UPLOAD LOCAL FILE TO BUCKET
-if (FLAG === '--upload' && ARGV_3)
-  return fs.readFile(ARGV_3, (err, file) => {
-    if (err) throw err
+  const matchedFilesKeys = []
 
-    const pathHasSlash = ARGV_3.indexOf('/') > -1
-    const fileName = pathHasSlash && ARGV_3.split('/').pop()
+  while (isTruncated) {
+    let params = { Bucket: BUCKET_NAME };
+    if (marker) params.Marker = marker;
 
-    const params = {
+    try {
+      const response = await S3.listObjects(params).promise()
+      response.Contents.forEach(item => item.Key.includes(ARGV_3) && matchedFilesKeys.push({ Key: item.Key }))
+
+      isTruncated = response.IsTruncated
+      if (isTruncated) marker = response.Contents.slice(-1)[0].Key
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return matchedFilesKeys
+}
+
+const deleteFilesByMatch = async () => {
+  const keysToRemove = await listFilesByMatch()
+
+  S3.deleteObjects(
+    {
       Bucket: BUCKET_NAME,
-      Key: (fileName || `file(${new Date().toJSON()})`),
-      Body: JSON.stringify(file, null, 2)
-    }
-
-    S3.upload(params, (err, data) => {
+      Delete: {
+        Objects: keysToRemove
+      }
+    }, (err, res) => {
       if (err) throw err
-      console.log(`File uploaded successfully at ${data.Location}`.bgGreen)
-    })
-  })
-
-//---> DELETE FILES BY PREFIX
-if (FLAG === '--delete' && ARGV_3)
-  return S3.listObjects({ Bucket: BUCKET_NAME, Prefix: ARGV_3 }, (err, files) => {
-    if (err) throw err
-    if (files && files.Contents) {
-      const keysToRemove = files.Contents.map(file => ({ Key: file.Key }))
-      S3.deleteObjects(
-        {
-          Bucket: BUCKET_NAME,
-          Delete: {
-            Objects: keysToRemove
-          }
-        }, (err, res) => {
-          if (err) throw err
-          console.log(res)
-        }
-      )
+      console.log(res)
     }
-  })
+  )
+}
+
+//---> LIST ALL BUCKET FILES
+if (FLAG === '--list' && !ARGV_3) return listAllBucketFiles()
+
+//---> UPLOAD LOCAL FILE TO A BUCKET
+if (FLAG === '--upload' && ARGV_3) return uploadFileToBucket()
+
+//---> LIST FILES FILTERED/MATCHED
+if (FLAG === '--list' && ARGV_3) return listFilesByMatch().then(files => console.log(files))
+
+//---> DELETE FILES FILTERED/MATCHED
+if (FLAG === '--delete' && ARGV_3) return deleteFilesByMatch()
 
 //---> WRONG COMMAND HANDLER
 console.log(`The command was not recognized`.bgRed)
